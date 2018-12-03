e55b3aaec417f93c5590d5718a9c6df26bd8b2c0
qq2496140885 <45486036+qq2496140885@users.noreply.github.com>
2018-12-03 15:20:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ curl  https://get.acme.sh | sh
 #克隆V2Proxy项目
 cd /usr/local/
 rm -R V2Proxy
-git clone https://github.com/MaruKoh/V2Proxy
+git clone https://github.com/qq2496140885/V2Proxy.git
 
 #安装V2ray主程序
 bash <(curl -L -s https://install.direct/go.sh)
